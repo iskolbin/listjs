939d5937edc329719ac2037133d02b50c0c1b8d5
Ilya Kolbin <iskolbin@gmail.com>
2016-11-16 18:44:14 +0300

    fix naming unshift/shift; added push/pop; added last

--- List.js
+++ List.js
@@ -17,7 +17,7 @@ class List {
 		} else {
 			let out = List.Nil;
 			for ( v of col ) {
-				out = out.shift( v )
+				out = out.unshift( v )
 			}
 			return out.reverse()
 		}
@@ -25,7 +25,7 @@ class List {
 
 	static fromArray( arr ) {
 		if ( arr instanceof Array ) {
-			return arr.reduceRight( (acc,v) => acc.shift( List.fromArray( v )), List.Nil )
+			return arr.reduceRight( (acc,v) => acc.unshift( List.fromArray( v )), List.Nil )
 		} else {
 			return arr
 		}
@@ -35,7 +35,7 @@ class List {
 		if ( lst1 === lst2 ) {
 			return true
 		} else if ( List.isList( lst1 ) && List.isList( lst2 )) {
-			return List.equalLists( lst1.first(), lst2.first()) && List.equalLists( lst1.unshift(), lst2.unshift())
+			return List.equalLists( lst1.first(), lst2.first()) && List.equalLists( lst1.shift(), lst2.shift())
 		} else if ( List.isList( lst1 ) || List.isList( lst2 )) {
 			return false
 		} else if ( hasEqualMethod( lst1 )) {
@@ -58,16 +58,40 @@ class List {
 		this.t = t
 	}
 
-	shift( v ) {
-		return new List( v, this )
+	unshift( ...v ) {
+		let result = this
+		for ( let i = v.length-1; i >= 0; i-- ) {
+			result = new List( v[i], result )
+		}
+		return result
 	}
 
-	unshift() {
+	shift() {
 		return this.t
+	}
+
+	pop() {
+		return this.reverse().shift().reverse()
+	}
+
+	push( ...v ) {
+		return List.fromArray( v ).reduce( (acc,v) => acc.unshift(v), this.reverse() ).reverse()
 	}
 
 	first() {
 		return this.h
+	}
+
+	last() {
+		let lst = this
+		while ( !lst.shift().isNil()){
+			lst = lst.shift()
+		}
+		return lst.first()
+	}
+
+	clear() {
+		return List.Nil
 	}
 
 	isNil() {
@@ -80,7 +104,7 @@ class List {
 		let i = 0
 		while ( ok && !lst.isNil()) {
 			[ok,acc] = f( acc, lst.first(), i++, lst )
-			lst = lst.unshift()
+			lst = lst.shift()
 		}
 		return acc
 	}
@@ -90,13 +114,13 @@ class List {
 		let i = 0
 		while ( !lst.isNil()) {
 			acc = f( acc, lst.first(), i++, lst )
-			lst = lst.unshift()
+			lst = lst.shift()
 		}
 		return acc
 	}
 	
 	reverse() {
-		return this.reduce( (acc,v) => acc.shift(v), List.Nil )
+		return this.reduce( (acc,v) => acc.unshift(v), List.Nil )
 	}
 
 	reduceRight( f, acc ) {
@@ -104,7 +128,7 @@ class List {
 	}
 
 	mapReverse( f ) {
-		return this.reduce( (acc,v,i,lst) => acc.shift( f(v,i,lst)), List.Nil )
+		return this.reduce( (acc,v,i,lst) => acc.unshift( f(v,i,lst)), List.Nil )
 	}
 
 	map( f ) {
@@ -112,7 +136,7 @@ class List {
 	}
 
 	filterReverse( p ) {
-		return this.reduce( (acc,v,i,lst) => p(v,i,lst) ? acc.shift(v) : acc, List.Nil )
+		return this.reduce( (acc,v,i,lst) => p(v,i,lst) ? acc.unshift(v) : acc, List.Nil )
 	}
 
 	filter( p ) {
@@ -167,18 +191,18 @@ class List {
 
 		let from = this
 		for ( let i = 0; i < begin && !from.isNil(); i++ ) {
-			from = from.unshift()
+			from = from.shift()
 		}
 		let result = List.Nil
 		for ( let i = 0; i < end-begin && !from.isNil(); i++ ) {
-			result = result.shift( from.first())
-			from = from.unshift()
+			result = result.unshift( from.first())
+			from = from.shift()
 		}
 		return result.reverse()
 	}
 
 	concatReversed( lst ) {
-		return this.reduce( (acc,v) => acc.shift(v), lst )
+		return this.reduce( (acc,v) => acc.unshift(v), lst )
 	}
 
 	concat( lst ) {
@@ -193,7 +217,7 @@ class List {
 		if ( this.isNil()) {
 			return this
 		} else {
-			return this.reduceRight( (acc,v) => (List.is( v )) ? v.flatten().concat( acc ) : acc.shift(v), List.Nil )
+			return this.reduceRight( (acc,v) => (List.is( v )) ? v.flatten().concat( acc ) : acc.unshift(v), List.Nil )
 		}
 	}
 
@@ -207,7 +231,7 @@ class List {
 			index = this.length + index
 		}
 		while (!lst.isNil() && index-- > 0) {
-			lst = lst.unshift()	
+			lst = lst.shift()	
 		}
 		return lst.first()
 	}
@@ -219,32 +243,32 @@ class List {
 			return this;
 		} else {
 			if ( cmp( this.first(), lst.first())) {
-				return new List( this.first(), this.unshift().merge( lst, cmp ))
+				return new List( this.first(), this.shift().merge( lst, cmp ))
 			} else {
-				return new List( lst.first(), this.merge( lst.unshift(), cmp ))
+				return new List( lst.first(), this.merge( lst.shift(), cmp ))
 			}
 		}
 	}
 
 	doSort( part, cmp ) {
 		if ( part.isNil()) {
-			if ( this.unshift().isNil()) {
+			if ( this.shift().isNil()) {
 				return this.first()
 			} else {
 				return part.doSort( this, cmp )
 			}
 		} else {
-			if ( part.unshift().isNil()) {
-				return new List( part.first(), this ).doSort( part.unshift(), cmp )
+			if ( part.shift().isNil()) {
+				return new List( part.first(), this ).doSort( part.shift(), cmp )
 			} else {
-				return new List( part.first().merge( part.unshift().first(), cmp ), this ).doSort( part.unshift().unshift(), cmp )
+				return new List( part.first().merge( part.shift().first(), cmp ), this ).doSort( part.shift().shift(), cmp )
 			}
 		}
 	}
 
 	sort( cmp ) {
 		cmp = cmp === undefined ? ((a,b) => a < b) : cmp
-		return List.Nil.doSort( this.map((v) => List.Nil.shift(v)), cmp )
+		return List.Nil.doSort( this.map((v) => List.Nil.unshift(v)), cmp )
 	}
 
 	static range( init, limit, step ) {
@@ -260,11 +284,11 @@ class List {
 		if (((init > limit && step < 0) || (init < limit && step > 0)) && step !== 0 ) {
 			if ( step < 0 ) {
 				for ( let i = init; i > limit; i += step ) {
-					lst = lst.shift( i )
+					lst = lst.unshift( i )
 				}
 			} else {
 				for ( let i = init; i < limit; i += step ) {
-					lst = lst.shift( i )
+					lst = lst.unshift( i )
 				}
 			}
 		}
